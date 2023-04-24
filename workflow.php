a27812c59aa516c49e9ab5942b7d00312496f389
ncastellanos21 <ncastellanos21@ilg.cat>
2023-04-24 15:59:36 +0200

    3rd Commit

--- workflow.php
+++ workflow.php
@@ -1,26 +1,33 @@
 <?php
+
+    function comprobador($id, $descriptor)
+    {
+        if (!empty($id) && !empty($descriptor)) {
+            $_SESSION["estat"] = '1';
+            $_SESSION["id"] = $id;
+            $_SESSION["description"] = $descriptor;
+        }
+    }
+
     session_start();
 
     if(!isset($_SESSION["estat"])) {
         $_SESSION["estat"] = "0";
-        include_once("vistaDades.php");
     } else {
         if(isset($_POST["next"])){
-            $_SESSION["estat"] = $_POST["next"];
+            if (isset($_POST["identifier"]) && isset($_POST["description"]) && $_SESSION["estat"] == '1') {
+                comprobador($_POST["identifier"], $_POST["description"]);
+            }
+            if (isset($_FILES["farmacos"]["name"]) && $_SESSION["estat"] == '2') {
+                move_uploaded_file($_FILES["farmacos"]["tmp_name"], "./files/".$_FILES["farmacos"]["name"]);
+                $_SESSION["rutaFarmacos"] = "./files/". $_FILES["farmacos"]["name"];
+            }
         } else {
             $_SESSION["estat"] = $_POST["back"];
         }
     }
 
-    if (isset($_POST["identifier"]) && isset($_POST["description"]) && $_SESSION["estat"] == '1') {
-        $_SESSION["id"] = $_POST["identifier"];
-        $_SESSION["description"] = $_POST["description"];
-    }
 
-    if (isset($_FILES["farmacos"]["name"]) && $_SESSION["estat"] == '2') {
-        move_uploaded_file($_FILES["farmacos"]["tmp_name"], "./files/".$_FILES["farmacos"]["name"]);
-        $_SESSION["rutaFarmacos"] = "./files/". $_FILES["farmacos"]["name"];
-    }
 
     if (isset($_FILES["proteinas"]["name"]) && $_SESSION["estat"] == '3') {
         move_uploaded_file($_FILES["proteinas"]["tmp_name"], "./files/".$_FILES["proteinas"]["name"]);
@@ -29,13 +36,13 @@
 
     // Cargar vista
 
-    if($_SESSION["estat"] == '0') {
-        include_once("vistaDades.php");
-    }
+    // if($_SESSION["estat"] == '0') {
+    //     include_once("vistaDades.php");
+    // }
 
-    if ($_SESSION["estat"] == '1') {
-        include_once("vistaFarmacs.php");
-    }
+    // if ($_SESSION["estat"] == '1') {
+    //     include_once("vistaFarmacs.php");
+    // }
 
     switch ($_SESSION["estat"]) {
         case '0':
